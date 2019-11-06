d10366b4b730dfdb710eb15df3eab896d9382016
MarkMili <markmilinovic24@gmail.com>
2019-11-06 14:48:31 +0100

    commit 1

---
+++ Program.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Program
+{
+  public static void main(String[] args)throws IOException
+  {
+    BufferedReader prva=new BufferedReader(new BufferedReader(new inputStreamReader(System.in)));
+    System.out.println("Vnesite poljubno celo stevilo:");
+      //random comment
+    String vnesi=prva.readLine();
+    //static Integer decode(String nm)
+    Integer stevilo=Integer.decode(prva);
+    int preveri=stevilo%5;
+    if (preveri=0)
+    {
+      System.out.println("To stevilo je veckratnik stevila 5.");
+    }
+    else
+    {
+      System.out.println("To stevilo ni veckratnik stevila 5.");
+    }
+  }
+}
