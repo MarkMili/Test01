320edd433d6a495cb04951778f302fee344ca0c7
MarkMili <markmilinovic24@gmail.com>
2019-11-06 15:44:31 +0100

    Imported scanner objekt

--- Program.java
+++ Program.java
@@ -5,6 +5,7 @@ public class Program
   public static void main(String[] args)throws IOException
   {
     BufferedReader prva=new BufferedReader(new BufferedReader(new inputStreamReader(System.in)));
+    Scanner scan=new Scanner(System.in);
     System.out.println("Vnesite poljubno celo stevilo:");
     String vnesi=prva.readLine();
     Integer stevilo=Integer.decode(prva);
